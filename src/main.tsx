6152c0c24783f8a802653130ce6d9f41f17304a1
Zach Anderson <zachbanderson@gmail.com>
2026-04-04 16:27:54 -0700

    Removed auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Authenticator } from "@aws-amplify/ui-react"
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
@@ -12,8 +11,6 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>
 );
